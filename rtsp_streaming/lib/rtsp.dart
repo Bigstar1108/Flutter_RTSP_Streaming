c189d5ae001abd420f45916fca6d5c355c11edcc
Dae Seong <daesung09854@gmail.com>
2021-01-26 12:55:35 +0900

    [DONE]

--- rtsp_streaming/lib/rtsp.dart
+++ rtsp_streaming/lib/rtsp.dart
@@ -1,4 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' show Client;
+import 'package:http/http.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
@@ -10,6 +13,8 @@ class RtspStreaming extends StatefulWidget {
 
 class _RtspStreaming extends State<RtspStreaming> {
   String initUrl = "rtsp://192.168.1.73:8554/mystream";
+
+  Timer _timer;
 
   String changeUrl =
       "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4";
@@ -23,47 +28,52 @@ class _RtspStreaming extends State<RtspStreaming> {
       _videoViewController.play();
     });
 
-    // _videoViewController.addListener(() {
-    //   switch (_videoViewController.playingState) {
-    //     // case PlayingState.PAUSED:
-    //     //   setState(() {
-    //     //     isBuffering = false;
-    //     //   });
-    //     //   break;
-    //     case PlayingState.STOPPED:
-    //       setState(() {
-    //         isBuffering = true;
-    //       });
-    //       break;
-    //     case PlayingState.BUFFERING:
-    //       setState(() {
-    //         isBuffering = true;
-    //       });
-    //       break;
-    //     case PlayingState.PLAYING:
-    //       setState(() {
-    //         isBuffering = false;
-    //       });
-    //       break;
-    //     case PlayingState.ERROR:
-    //       setState(() {
-    //         isBuffering = true;
-    //       });
-    //       print("VLC encountered error");
-    //       break;
-    //     default:
-    //       setState(() {});
-    //       break;
-    //   }
-    // });
+    // controller에 addlistener를 달아서, 예외처리 진행
+    _videoViewController.addListener(() {
+      switch (_videoViewController.playingState) {
+        case PlayingState.STOPPED:
+          resetPlayer();
+          break;
+        case PlayingState.BUFFERING:
+          resetPlayer();
+          break;
+        case PlayingState.PLAYING:
+          setState(() {
+            isBuffering = false;
+          });
+          if (_timer != null) {
+            _timer.cancel();
+            print("timer playing");
+          }
+          break;
+        case PlayingState.ERROR:
+          resetPlayer();
+          print("VLC encountered error");
+          break;
+        default:
+          setState(() {});
+          break;
+      }
+    });
 
     super.initState();
   }
 
+  // vlc player를 초기화하고 재시작하는 함수
+  resetPlayer() {
+    setState(() {
+      isBuffering = true;
+    });
+    _videoViewController.dispose();
+    _timer = Timer.periodic(new Duration(seconds: 1), (timer) async {
+      _videoViewController.play();
+    });
+  }
+
+  // RTSP 재생을 위한 vlc player를 return하는 함수
   showPlaying() {
     return Column(
       children: <Widget>[
-        // _videoViewController.isPlaying() != null ? Text('connecting...') : Text('playing'),
         isBuffering ? Text('connecting...') : Text('playing'),
         SizedBox(
           height: 250,
@@ -76,7 +86,7 @@ class _RtspStreaming extends State<RtspStreaming> {
               '--quiet',
               '--no-drop-late-frames',
               '--no-skip-frames',
-              '--no-rtsp-tcp', // tcp로 받지 않겠다.
+              '--no-rtsp-tcp',
             ],
             hwAcc: HwAcc.DISABLED,
             placeholder: Container(
@@ -92,6 +102,30 @@ class _RtspStreaming extends State<RtspStreaming> {
     );
   }
 
+  // ignore: missing_return
+  Future<List> getPortSpeed() async {
+    Client client = Client();
+    var url = "https://seadronix.jp.ngrok.io/get/portspeed";
+
+    // var token = '';
+
+    try {
+      Map data = {
+        "harbor": ["KR_USN"],
+        "dock": "silo"
+      };
+
+      var body = json.encode(data);
+
+      final response = await client.post(url,
+          headers: {"Content-Type": "application/json"}, body: body);
+
+      print(response.body);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -104,6 +138,21 @@ class _RtspStreaming extends State<RtspStreaming> {
             child: Column(
               children: <Widget>[
                 showPlaying(),
+                GestureDetector(
+                  onTap: () {
+                    getPortSpeed();
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: 70,
+                    alignment: Alignment.center,
+                    color: Colors.black,
+                    child: Text(
+                      "TEST",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                )
               ],
             ));
       }),
@@ -113,6 +162,7 @@ class _RtspStreaming extends State<RtspStreaming> {
   @override
   void dispose() {
     _videoViewController.dispose();
+    _timer.cancel();
     super.dispose();
   }
 }
